78f0c01c6095482766e410a2648e24944273c6b6
Niels Holt <niels@steenbuchholt.dk>
2020-10-28 14:56:44 +0100

    Using fcoo/leaflet-bugfix to include latest version of leaflet

--- demo/bower_components.js
+++ demo/bower_components.js
@@ -20011,6 +20011,62 @@ else {
 //# sourceMappingURL=bootstrap.bundle.js.map
 
 ;
+/****************************************************************************
+	leaflet-global-switches.js,
+
+	(c) 2020, FCOO
+
+	https://github.com/FCOO/leaflet-global-switches
+	https://github.com/FCOO
+
+    Setting global switches for leaflet
+
+****************************************************************************/
+(function (window/*, document, undefined*/) {
+	"use strict";
+
+    //Detect Operation System. Possible values = "windows_phone", "android", "ios", null
+    var os = function() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return "windows_phone";
+        }
+
+        if (/android/i.test(userAgent)) {
+            return "android";
+        }
+
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "ios";
+        }
+
+        return null;//"unknown";
+    }(),
+    //To use later: is_windows_phone = (os == "windows_phone"),
+    //To use later: is_android = (os == "android"),
+    is_ios = (os == "ios");
+
+    /**************************************************
+    Global Switches for Leaflet
+    From Leaflet 1.2.0:
+    Switch	        Description
+    L_NO_TOUCH	    Forces Leaflet to not use touch events even if it detects them.
+    L_DISABLE_3D	Forces Leaflet to not use hardware-accelerated CSS 3D transforms for positioning (which may cause glitches in some rare environments) even if they're supported.
+    **************************************************/
+
+    //Turn off 3D transform for ios
+    window.L_DISABLE_3D = is_ios;
+
+
+}(this, document));
+
+
+
+
+;
 /* @preserve
  * Leaflet 1.7.1, a JS library for interactive maps. http://leafletjs.com
  * (c) 2010-2019 Vladimir Agafonkin, (c) 2010-2011 CloudMade
@@ -100652,6 +100708,51 @@ Is adjusted fork of Touch-Menu-Like-Android (https://github.com/ericktatsui/Touc
     };
 
 }(jQuery, this.Hammer, this, document));
+;
+/****************************************************************************
+    leaflet-bugfix.js,
+
+    (c) 2020, FCOO
+
+    https://github.com/FCOO/leaflet-bugfix
+    https://github.com/FCOO
+
+****************************************************************************/
+(function (L/*, window, document, undefined*/) {
+    "use strict";
+
+    /*
+    Fix bug intruduced in Leaflet 1.5
+    Version 1.7.1 is
+    L.Path.prototype.setStyle = function (style) {
+          setOptions(this, style);
+          if (this._renderer) {
+              this._renderer._updateStyle(this);
+              if (this.options.stroke && style && Object.prototype.hasOwnProperty.call(style, 'weight')) {
+                  this.this._updateBounds();
+              }
+          }
+          return this;
+      };
+    */
+
+    L.Path.prototype.setStyle = function (style) {
+        this.options = $.extend(this.options, style);
+          if (this._renderer) {
+              this._renderer._updateStyle(this);
+              if (this.options.stroke && style && Object.prototype.hasOwnProperty.call(style, 'weight')) {
+                  this._project();
+              }
+          }
+          return this;
+      };
+
+
+}(L, this, document));
+
+
+
+
 ;
 /*****************************************************************************
 leaflet-latlng-geodesy.js
